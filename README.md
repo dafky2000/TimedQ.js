1d349de64f32c8e82ca511ef9c3dc17bbd45b19f
Dan Kelly <myself@danielkelly.me>
2017-07-17 10:32:19 -0400

    Initial commit with LICENSE, README and initial codebase

---
+++ README.md
@@ -0,0 +1,94 @@
+# TimedQ.js
+
+Javascript timed processing queue (and queue manager). Useful as a non render blocking processing queue in the browser. 
+
+A simple queue manager to call batches of functions and data in timed blocks. The amount of data to be processed is self adjusted based on the function call time. The primary goal is to provide an easy to use, non render blocking processing queues so the browser does not lock up during large tasks with very little overhead. There is certainly a better way to explain this...
+
+# Assumptions / Requirements
+
+* ES6
+* Your processing function doesn't take longer than the `runtime`
+
+# TODO
+
+* Unit testing
+* API Documentation
+* Benchmarking
+
+# Download source
+
+```bash
+git clone https://github.com/dafky2000/TimedQ.js
+```
+
+# Include in your project
+
+Add to the head tag in your project
+
+```html
+<script src="TimedQ.js" async></script>
+```
+
+# Initialize & start TimedQ
+
+Initializes and starts the queue manager, checks every 100ms (configurable with idletime) until items are `enqueued`.
+
+Using (optional) options ('sane' defaults shown below)
+
+```javascript
+const Q = new TimedQ({
+  runtime: 40,    // How long to process items before taking a break
+  breaktime: 1,   // How long to break between processing blocks
+  idletime: 100,  // How long to wait between processing blocks if there was nothing to process
+});
+```
+
+# TimedQ.prototype.enqueue(**signature**, **data**, **options**)
+
+(required) **signature**: Typically a function which is used as the identifier for the queue group as well as the function to call on the dataset. Otherwise it could be another string, int, float, object reference... whatever can be used as a `Map()` key but you will need to define a callback in the options if not a function.
+
+(required) **data**: 2-dimensional array of parameters to supply to function.
+
+(optional) **optional**: {
+  callback: function(param1, param2...) {},
+} 
+
+# Queue items to be run
+
+Queue an inline function to run on a set of data
+
+```javascript
+Q.enqueue(function(param1, param2) { console.log(param1 + param2); }, [
+  ['data for param1','data for param2'],
+  ['more data for param1','more data for param2'],
+  ['even more data for param1','even more data for param2'],
+]);
+```
+
+Queue a defined function
+
+```javascript
+function fun(param1, param2) {
+  console.log(param1 + param2);
+}
+
+Q.enqueue(fun, [
+  ['data for param1','data for param2'],
+  ['more data for param1','more data for param2'],
+  ['even more data for param1','even more data for param2'],
+]);
+```
+
+Queueing data to a specific queue name
+
+```javascript
+Q.enqueue('my_queue1', [
+  ['data for param1','data for param2'],
+  ['more data for param1','more data for param2'],
+  ['even more data for param1','even more data for param2'],
+], {
+  callback: function(param1, param2) {
+    console.log(param1 + param2);
+  },
+});
+```
---
+++ TimedQ.js
@@ -0,0 +1,190 @@
+function TimedQ(options = {}) {
+	this.options = {
+		runtime: options.runtime || 40,
+		breaktime: options.breaktime || 1,
+		idletime: options.idletime || 100,
+	};
+	this.timer = null;
+	this.queues = new Map(); // Maps signature to qgroup
+	this.queues_array = []; // Array of added Queues
+
+	this.start();
+}
+
+TimedQ.prototype.qgroup = function(signature, index, options={}) {
+	this.index = index;
+	this.signature = signature;
+	this.fnc = options.callback || (typeof(signature) === 'function' ? signature : false) || undefined;
+	this.items = [];
+	this.totaldequeued = 0;
+	this.totalenqueued = 0;
+	this.finishcount = 0;
+	this.timing = {
+		created: new Date().getTime(),
+		start: 0,
+		finish: 0,
+		avg: 0,
+		min: 0,
+		max: 0,
+	};
+};
+
+TimedQ.prototype.get = function(sig, callback) {
+	// Get the existing queue
+	let q = this.queues.get(sig);
+
+	// If this queue doesn't already exist, create it and return it
+	if(!q) {
+		q = this.queues.set(sig, new this.qgroup(sig, this.queues_array.length, {
+											callback: callback,
+										}))
+									 .get(sig);
+		this.queues_array.push(q);
+	}
+
+	return q;
+};
+
+/*
+	data should always be an array of an array of parameters passed to the function. (2 dimensional array for adding batches of data to be processed)
+	options = {
+		callback: function(param1, param2) {}, // If different from signature
+		TODO: type: 'FIFO', // FIFO, LIFO
+	};
+*/
+TimedQ.prototype.enqueue = function(sig, data=[], options={}) {
+	// Get (or create) the this.qgroup
+	const q = this.get(sig, options.callback);
+
+	// Now return, in case we just wanted to create the this.qgroup
+	if(!data.length) return;
+
+	// Push the data to the end of the queue
+	q.items = q.items.concat(data);
+
+	++q.totalenqueued;
+
+	// console.log(data.length + ' items added to queue index ' + q.index + '. ' + q.items.length + ' total items now');
+
+	// TODO: Set q.timing.start if the queue is not already running.
+
+	return true;
+};
+
+TimedQ.prototype.enqueue_one = function(sig, data, options={}) {
+	return this.enqueue(sig, [data], options);
+};
+
+TimedQ.prototype.dequeue = function(sig) {
+	const q = this.queues.get(sig);
+
+	if(!q || !q.items.length) return undefined;
+
+	const qitem = q.items.pop();
+
+	++q.totaldequeued;
+
+	return qitem;
+};
+
+TimedQ.prototype.start = function() {
+	clearTimeout(this.timer);
+	this.process();
+};
+
+TimedQ.prototype.stop = function() {
+	clearTimeout(this.timer);
+};
+
+TimedQ.prototype.process = function() {
+	// Register as running to checks immediately
+	this.timer = 1;
+
+	let totaltime = 0,
+			remaining = 0;
+
+	for(let q of this.queues_array) {
+		if(!q.items.length) continue;
+
+		// Determine if we need to start with the function once (first run) or
+		// If there is no timing data, just run once and time it.
+		let max = 1;
+		// if we can use the time data to determine a safe running count and do it
+		if(q.timing.avg)
+			max = Math.floor((this.options.runtime-totaltime) / q.timing.avg) || max;
+
+		// Run our queue for max number of times
+		const startdate = new Date().getTime();
+		let x = 0;
+		for(; x < max && q.items.length; ++x)
+			q.fnc.apply(q, this.dequeue(q.signature));
+		const enddate = new Date().getTime();
+
+		// Set some timing data
+		// In case the function takes less than one ms, we'll say it took 1ms
+		const curtotaltime = ((enddate - startdate) || 1);
+		// Avg runtime for each function in this run
+		const curavg = curtotaltime / x;
+		totaltime += curtotaltime;
+		// TODO: This if/else should be simpler, one code path?
+		if(!q.timing.avg) {
+			q.timing.avg = curavg;
+			q.timing.min = curavg;
+			q.timing.max = curavg;
+		} else {
+			q.timing.avg = (q.timing.avg * (q.totaldequeued - x) + curtotaltime) / q.totaldequeued;
+			q.timing.min = Math.min(q.timing.min, curavg);
+			q.timing.max = Math.max(q.timing.max, curavg);
+		}
+
+		// How many queued items remaining in this queue
+		remaining += q.items.length;
+
+		// If we just finished processing this queue, log/fire appropriate events
+		if(!q.items.length) {
+			this.finishcount = this.totaldequeued;
+		}
+
+		// If we've exceeded our total runtime, break
+		if(totaltime > this.options.runtime - 1) break;
+	}
+
+	// If there are more items to process continue, else wait 100ms and check again
+	if(remaining) this.timer = setTimeout(this.process.bind(this), this.options.breaktime);
+	else this.timer = setTimeout(this.process.bind(this), this.options.idletime);
+};
+
+function runtests() {
+	const Q = new TimedQ();
+
+	// TESTS
+	const f = function(input) {
+		 let newtext = 'f1' + input;
+		 newtext += ' another addition';
+		 newtext += ' another addition2';
+		 newtext += ' another addition3';
+		 return newtext;
+	};
+
+	const f2 = function(input) {
+		 let newtext = 'f2' + input;
+		 newtext += ' another addition';
+		 newtext += ' another addition2';
+		 newtext += ' another addition3';
+		 return newtext;
+	};
+
+	let data = [];
+	for(let x = 0; x < 1000000; ++x) {
+		data.push(['test'+x]);
+	}
+
+	for(let x = 0; x < 10; ++x) {
+		Q.enqueue(f, data);
+		Q.enqueue(f2, data);
+	}
+
+	console.log('Done adding items to the queues');
+}
+
+runtests();
